9c05b47e5e27f9888fd32955c4f2f207b271e47e
Skeptical <161164507+skepticalmc@users.noreply.github.com>
2025-02-11 16:00:05 +0000

    Export all structures

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 export { Client as default } from "./structures/Client";
 
+export * from "./structures/Ban";
 export * from "./structures/Client";
-export * from "./structures/User";
+export * from "./structures/Member";
 export * from "./structures/Message";
+export * from "./structures/Server";
+export * from "./structures/User";
 
 export * from "./gateway/WebSocketManager";
 export * from "./gateway/events";
